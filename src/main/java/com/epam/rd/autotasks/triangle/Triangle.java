88f05ee5d2467f92905908e62cdec6a18db046c3
Artsiom Trushko <artemchiik@gmail.com>
2022-05-09 15:11:06 +0300

    solved

--- src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
+++ src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
@@ -1,17 +1,46 @@
 package com.epam.rd.autotasks.triangle;
 
+import static java.lang.Math.sqrt;
+import static java.lang.StrictMath.pow;
+
 class Triangle {
+    Point a;
+    Point b;
+    Point c;
 
     public Triangle(Point a, Point b, Point c) {
-        //TODO
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        double one = (Math.sqrt(Math.pow((b.getX() - a.getX()),2) + pow((b.getY() - a.getY()),2)));
+        double two = (Math.sqrt(Math.pow((c.getX() - b.getX()),2) + pow((c.getY() - b.getY()),2)));
+        double three = (Math.sqrt(Math.pow((c.getX() - a.getX()),2) + pow((c.getY() - a.getY()),2)));
+
+        if (((one + two) < three) || ((one + three) < two) || ((two + three) < one) ){
+            throw new IllegalArgumentException();
+        }
+
+        if (((c.getX() - a.getX()) / (b.getX() - a.getX())) == ((c.getY() - a.getY()) / (b.getY() - a.getY()))){
+            throw new IllegalArgumentException();
+        }
+
+        if ((a.getY() == b.getY()) && (a.getY() == c.getY())){
+            throw new IllegalArgumentException();
+        }
+
+        if ((a.getX() == b.getX()) && (a.getX() == c.getX())){
+            throw new IllegalArgumentException();
+        }
+
     }
 
     public double area() {
-        //TODO
+        return ((Math.abs((a.getX() - c.getX()) * (b.getY() - a.getY()) - (a.getX() - b.getX()) * (c.getY() - a.getY()))) / 2);
     }
 
     public Point centroid(){
-        //TODO
+        return (new Point(( (a.getX() + b.getX() + c.getX()) / 3), ( (a.getY() + b.getY() + c.getY()) / 3)));
     }
 
 }
